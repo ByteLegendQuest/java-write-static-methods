c62a5921a6d53932b46582fc1ee113fc288e89d3
GergertHatby <yanghao9225@outlook.com>
2022-06-06 13:16:07 +0000

    Update at 2022-06-06T13:16:07.957897Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,15 +2,7 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
-        System.out.println(hello());
-        System.out.println(add(40, 2));
-    }
-
-    public static String hello() {
-        return "Hello";
-    }
-
-    public static int add(int z,int y) {
-        return z + y;
+        System.out.println(Hello);
+        System.out.println(42);
     }
 }
