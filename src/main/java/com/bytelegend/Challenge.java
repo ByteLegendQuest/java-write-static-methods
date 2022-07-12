0288eb4d8ec8333f32f4e96c28bb7230757a2939
sorahb <sorahbinc@gmail.com>
2022-07-12 02:32:39 +0000

    Update at 2022-07-12T02:32:39.240241Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,7 +2,7 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("Hello");
         System.out.println(40 + 2);
     }
 
